47d5561354f0b2c8f84628c74aeda547c258cb6a
gongjian <15031617367@163.com>
2023-12-19 13:09:17 +0800

    分类模块数据格式优化，菜单逻辑判断

--- entry/src/main/ets/models/category/productList.ts
+++ entry/src/main/ets/models/category/productList.ts
@@ -1,16 +1,19 @@
 /*商品类型约束*/
 export interface ProductItem{
   tag:string,
+  key:string,
   data:ProductItemData[]
 }
 
 
 export interface ProductItemData{
   subTag:string,
+  key:string,
   subData:ProductItemSubData[]
 }
 
 export interface ProductItemSubData {
+  key:string,
   title:string,
   price:number,
   tage:Boolean,
